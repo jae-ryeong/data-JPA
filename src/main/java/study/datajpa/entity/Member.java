16645a629d130066616d1e6e2ecd995ff9ac2dff
김재령 <wofud0321@naver.com>
2023-03-22 19:51:02 +0900

    예제 도메인 모델

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,27 +1,40 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
-    protected Member() {    // JPA 표준 스펙에 의해 Entity는 아무 쓸모없는 기본 생성자가 있어야한다.
-        // JPA는 프록시 기술을 사용하기 때문에
-    }
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")   //FK의 이름이 된다.
+    private Team team;
+
+
     public Member(String username) {
         this.username = username;
+    } // public void change_username(String username)  이름바꾸는게 필요하면 이런 메서드를 만들면된다.
+
+    public Member(String usename, int age, Team team) {
+        this.username = usename;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
     }
 
-    // public void change_username(String username)  이름바꾸는게 필요하면 이런 메서드를 만들면된다.
-
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }
