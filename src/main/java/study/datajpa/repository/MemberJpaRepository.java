adac0a3334225ba8cd7037cac775ad8733143630
김재령 <wofud0321@naver.com>
2023-03-22 22:09:04 +0900

    인터페이스 분석까지

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext // 스프링부트가 스프링 컨테이너와
@@ -15,6 +18,23 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findALl() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult(); // 단건 조회에 경우에는 getSingResult()를 사용하면 된다.
+    }
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
